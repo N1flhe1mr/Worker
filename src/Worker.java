8cc9072acd77e369451916328cd2d7a24ea1cdc3
Dmitriy_Korchagin <793070407232@ya.ru>
2023-02-02 00:33:09 +0300

    Ðhomework complete

--- src/Worker.java
+++ src/Worker.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Worker {
+public class Worker {
+    final private OnTaskDoneListener callback;
+    final private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i <= 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " completed with an error!");
+            } else {
+                callback.onDone("Task " + i + " is done!");
+            }
+        }
+    }
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
 }
